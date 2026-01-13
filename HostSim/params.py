4ab6da76689b011e8c7e24d42fd453ccc75890a4
Harald <harald.n.bauer@web.de>
2026-01-13 10:38:27 +0100

    Added params.py

--- HostSim/params.py
+++ HostSim/params.py
@@ -1,3 +1,15 @@
+import numpy as np
+
 # angle range = -120 ... 120 = -LidarMaxAngle ... LidarMaxAngle
 LidarMaxAngle = 120
 LidarFreq_Hz = 10
+
+config = 1
+if config==1:
+    RobotInitX = 15.00
+    RobotInitY = 7.50
+    RobotInitTheta =  -np.pi+3/4*np.pi
+else:
+    RobotInitX = 0.0
+    RobotInitY = 0.0
+    RobotInitTheta =  -np.pi
