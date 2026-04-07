83c84c60244486528a6d712b4f5e595640e626bd
Harald Bauer WSL <harald.n.bauer@web.de>
2026-04-07 09:20:04 +0200

    improved CYCLONEDDS_URI handling for Orin-NX

--- ros2/l4t/goto-docker.sh
+++ ros2/l4t/goto-docker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 sudo systemctl start eKarrenEmulator
-sudo docker exec -it ros2_dev_container bash -c "source /root/ros2/l4t/start-ros.bash && bash"
+source /home/harald/orin-git/ros2/l4t/generate_orin_cdds_uri.sh
+sudo docker exec -it \
+    -e CYCLONEDDS_URI="$CYCLONEDDS_URI" \
+    ros2_dev_container bash -c "source /root/ros2/l4t/start-ros.bash && bash"
 
