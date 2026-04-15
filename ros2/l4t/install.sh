f954d459e4175055060c8541151ea1cfe46eb21c
Harald Bauer <harald.n.bauer@web.de>
2026-04-15 15:57:27 +0200

    launch file debugging

--- ros2/l4t/install.sh
+++ ros2/l4t/install.sh
@@ -1,3 +1,4 @@
 sudo ln -si /home/harald/orin-git/ros2/l4t/ekarren.sh  /usr/local/bin/ekarren
 sudo ln -si /home/harald/orin-git/ros2/l4t/eksim.sh    /usr/local/bin/eksim
 sudo ln -si /home/harald/orin-git/ros2/l4t/ekemu.sh    /usr/local/bin/ekemu
+sudo ln -si /home/harald/orin-git/ros2/l4t/ekstop.sh   /usr/local/bin/ekstop
