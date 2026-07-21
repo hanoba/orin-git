1cbfe14c86d5a4c19fa5aa7cd55c6e3206a76118
Harald (raspi) <harald.n.bauer@web.de>
2026-07-21 20:22:52 +0200

    new U_MowTask

--- ekarren/U_MowTask.py
+++ ekarren/U_MowTask.py
@@ -2,7 +2,7 @@
 import numpy as np
 import math
 from params import LidarMaxAngle, TaskState, Ardumower
-from helper import GetSectorMedian
+import helper as h
 
 # Zustände
 FOLLOW = 0
@@ -11,17 +11,16 @@ NONE   = 2
 
 class U_MowTask:
     def Init(self, navigator, params, retvals=None):
-        
-        self.cos45 = math.cos(math.radians(45))   # winkel zwischen d_wall und d_wall45
-        self.SQRT_2 = math.sqrt(2.0)
+        self.gamma_deg = 70
+        gamma = math.radians(self.gamma_deg)
+        self.cos_gamma = math.cos(gamma)   # winkel zwischen d_wall und d_wall45
+        self.tan_gamma = math.tan(gamma)
 
         # Basis-Vorwärtsgeschwindigkeit
         self.base_speed = 0.5
         
         self.debugFlag = True
         self.nav = navigator
-        self.subState = 0
-        self.laneDist = -0.2        
         
         
         # Maximal Winkelgeschwindigkeit
@@ -31,12 +30,13 @@ class U_MowTask:
         # PD-Gewichte
         #self.K_lat  = 2.0  # 2.0  # lateral error (Abstand)
         #self.K_head = self.K_lat*0.5  # 1.0 # heading error (Wandorientierung)
-        self.K_lat  = 0.5  # 2.0 2.0  # lateral error (Abstand)
+        self.K_lat  = 1.0    # 0.5   # lateral error (Abstand)
         self.K_head = 0.1*0  # 0.3 1.0 # heading error (Wandorientierung)
 
         # Mähbereich
-        self.startDist = 5.6
+        self.startDist = 4.6
         self.endDist = 1.0
+        self.laneDist = -0.2        
         self.forwardTurnDist = 1.5
         self.backwardTurnDist = 12.0
         self.wallAngle = np.radians(85)
@@ -46,10 +46,18 @@ class U_MowTask:
         self.forward = True
 
         self.state = NONE
-        self.nav.SetWantedTheta(self.wallAngle)
+        #self.nav.SetWantedTheta(self.wallAngle)
         self.SetState(TURN)
 
         self.time = 0
+
+        self.headErrSum = 0.0
+        self.latErrSum = 0.0        
+        
+        self.turnCnt = 0
+        self.turnCntMax = 5
+        self.nav.SetMode(8)
+
 
     def GetStateText(self, newState):
         if newState==FOLLOW: return "FOLLOW"
@@ -61,26 +69,11 @@ class U_MowTask:
             if self.debugFlag: print(f"State: {self.GetStateText(newState)}")
             self.state = newState
 
-    ## Hilfsfunktion: Sektor mit Wraparound in Grad
-    #def GetSectorMin_deg(self, ranges_np, start_deg, end_deg):
-    #    start_deg += LidarMaxAngle
-    #    end_deg += LidarMaxAngle
-    #    """Gibt Minimalradius in einen Bereich in [start_deg, end_deg) zurück."""
-    #    if start_deg < end_deg: return np.nanmin(ranges_np[start_deg:end_deg]) 
-    #    elif start_deg < end_deg: return self.maxDist_mm
-    #    min1 = np.nanmin(ranges_np[start_deg:])
-    #    min2 = np.nanmin(ranges_np[:end_deg])
-    #    return min(min1, min2)  
-        
-
+    # Step wird immer aufgerufen, wenn neue Lidar-Daten verfügbar sind.
     def Step(self, ranges):
         #r = np.array(ranges, dtype=float)
         assert ranges.size == 2*LidarMaxAngle
 
-        # Inf / 0 als NaN behandeln
-        ranges[~np.isfinite(ranges)] = np.nan
-        ranges[ranges <= 0.0] = np.nan
-        
         d_zaun = 0
         angular = 0
         linear = 0
@@ -90,61 +83,64 @@ class U_MowTask:
         heading_error = 0
         omega = 0
         if self.state == TURN:
-            if self.nav.wantedThetaReached:
+            print(f"{self.headErrSum=}  {self.latErrSum=}")
+            self.headErrSum = 0.0
+            self.latErrSum = 0.0      
+            self.turnCnt += 1
+            self.nav.SetVelocities(0.0, 0.0)
+            if self.turnCnt >= self.turnCntMax:
                 self.SetState(FOLLOW)
+
                 
         elif self.state == FOLLOW:
             # --- Sektoren (Standard: 0° vorne, 90° links, 270° rechts) ---
             a = 5
+            d_zaun   = h.GetSectorMedian(ranges,   0, 2*a) 
+            d_wall   = h.GetSectorMin(ranges, -90-a,-90+a)  
+            d_wall_gamma = h.GetSectorMin(ranges, -(180-self.gamma_deg)-a, -(180-self.gamma_deg)+a)     # Messung bei Gamma
+            if Ardumower:
+                # Lidarsensor virtuell um C virtuell nach vorne verschieben
+                A = d_wall
+                B = d_wall_gamma
+                C = 0.5
+                # m ist die Steigung der Geraden (=Wall)
+                m = self.tan_gamma - A / (B*self.cos_gamma)
+                cos_theta = 1.0 / math.sqrt(1.0 + m**2)
+                d_wall = A * cos_theta
+                d_wall_new = (A - C*m)*cos_theta
+
             if self.forward:
                 s = 1.0
-                d_zaun   = GetSectorMedian(ranges,   0, 2*a) 
-                d_wall   = GetSectorMedian(ranges, -90-a,-90+a)  
-                d_wall45 = GetSectorMedian(ranges, -135-a, -135+a)     # Wandmessung um 45° versetzt
+                # Lateral-Fehler: Abstand zur Wand
+                lateral_error = self.target_dist - d_wall_new
+                self.K_lat  = 0.35    # 0.5   # lateral error (Abstand)
+                self.K_head = 0.60    # 0.3 1.0 # heading error (Wandorientierung)
             else:
                 s = -1.0
-                #d_zaun = min(GetSectorMedian(ranges, 180-a,  180),
-                #             GetSectorMedian(ranges,  -179, -179+a))
-                d_zaun = GetSectorMedian(ranges,  -179, -179+2*a)
-                d_wall     = GetSectorMedian(ranges,  90-a, 90+a)  
-                d_wall45 = GetSectorMedian(ranges,    45-a, 45+a)  
+                # Lateral-Fehler: Abstand zur Wand
+                lateral_error = self.target_dist - d_wall
+                self.K_lat  = 0.35    # 0.5   # lateral error (Abstand)
+                self.K_head = 0.60    # 0.3 1.0 # heading error (Wandorientierung)
 
-            # Nur wenn Wandmessung vernünftig
-            assert np.isfinite(d_wall)
-            assert np.isfinite(d_wall45)
+            # --- 5. Perfekt abgestimmte Regler-Werte (Kritische Dämpfung) ---
+            #self.K_lat  = 1.0  
+            #self.K_head = 1.5 
 
-            if Ardumower:
-                # Lidarsensor virtuell um C nach vorne verschieben
-                A = d_wall
-                B = d_wall45
-                C = 0.5
-                #H = A / math.sqrt(2.0)
-                #alpha = math.atan(B/H - 1)
-                #beta = alpha - math.pi/4
-                #d_wall += C*math.tan(beta)
-                
-                # Ohne Trigonometrie direkt den Faktor berechnen
-                tan_beta = 1.0 - (A * self.SQRT_2/B)
-                d_wall += C * tan_beta            
-            
-            # Lateral-Fehler: Abstand zur Wand
-            d_wall = self.target_dist - d_wall
-
-            # Heading-Fehler: Wandgeometrie (Wand schräg?)
             # Wenn right_front näher als right → Wand läuft schräg nach vorn → links drehen.
-            heading_error = d_wall - d_wall45*self.cos45
+            heading_error = C*m   #d_wall - d_wall_new
+            self.headErrSum += float(heading_error)
+            self.latErrSum += float(lateral_error)
             
-            omega = s*(self.K_lat * lateral_error - self.K_head * heading_error)
+            omega = s*self.K_lat*lateral_error + self.K_head*heading_error
             angular = omega
             if angular > self.max_angular: angular = self.max_angular
-            elif angular <- self.max_angular: angular = -self.max_angular
-            linear = self.base_speed
-
-            self.nav.SetVelocities(angular, linear) 
+            elif angular < -self.max_angular: angular = -self.max_angular
+            linear = s*self.base_speed
 
             if self.debugFlag:
-                print(f"{self.time:6d}: fwd={self.forward} {self.GetStateText(self.state)} d(0°)={d_zaun:.2f}  d(90°)={d_wall:.2f}  d(45°)={d_wall45:.2f}  "
-                f"latErr={lateral_error:5.2f}  headErr={heading_error:5.2f}  ang={angular:5.2f}  lin={linear:.2f}  tdist={self.target_dist:5.2f}")
+                headErr = math.degrees(math.atan(m))
+                print(f"{self.time:6d}: fwd={self.forward} {self.GetStateText(self.state)} d(0°)={d_zaun:.2f}  d(90°)={d_wall:.2f}  d(γ)={d_wall_gamma:.2f}  "
+                f"latErr={lateral_error:5.2f}  headErr={headErr:5.1f}°  ang={angular:5.2f}  lin={linear:.2f}  tdist={self.target_dist:5.2f}")
 
             if self.target_dist < 1.0:
                 #self.state = self.StateIdle
@@ -152,20 +148,24 @@ class U_MowTask:
                 self.nav.RvizPrint("Mowing completed")
                 return TaskState.Ready, None
 
-            vLinear = 0.2
             if self.forward:
                 if d_zaun < self.forwardTurnDist:   # 8
                     self.nav.SetVelocities(0.0, 0.0)
                     self.forward = False
                     self.target_dist += self.laneDist
-                    self.nav.SetWantedTheta(self.nav.theta - np.pi, vLinear, turnRight=True)
+                    self.turnCnt = 0
                     self.SetState(TURN)
             elif d_zaun > self.backwardTurnDist:  # 12.0:
                     self.nav.SetVelocities(0.0, 0.0)
                     self.forward = True
-                    self.target_dist += self.laneDist
-                    self.nav.SetWantedTheta(self.nav.theta + np.pi, vLinear, turnLeft=True)
+                    #self.target_dist += self.laneDist
+                    self.turnCnt = 0
                     self.SetState(TURN)
+                    
+            if self.state == TURN:
+                self.nav.SetVelocities(0.0, 0.0)
+            else:
+                self.nav.SetVelocities(angular, linear) 
+            
         self.time += 1
         return TaskState.Running, None
-
